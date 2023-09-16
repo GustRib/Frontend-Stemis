986a037526b97eeb1f35b890049d71b5affd1c51
GustRib <gu_crv1@hotmail.com>
2023-09-16 03:44:11 -0300

    Tailwind funcionando

--- Drinkeria/tailwind.config.js
+++ Drinkeria/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-],
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
