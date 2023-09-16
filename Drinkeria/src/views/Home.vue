0daf3e08c9d0b548cadb46b4ce78c4a119e175f8
GustRib <gu_crv1@hotmail.com>
2023-09-15 22:18:57 -0300

    criando a vue router e configurando suas rotas, criando o store junto com seus metodos utilizando o Vuex, e por fim adicionando o tailwind ao projeto(nao está funcionando, corrigir)

---
+++ Drinkeria/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <pre>{{ drinks }}</pre>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+
+const drinks = computed(() => store.state.drinks)
+
+</script>
